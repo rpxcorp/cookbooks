4fdca0bef6ef7a632edc65decf4183474c3b7ea7
sysops <sysops@rpxcorp.com>
2010-12-15 11:48:08 -0800

    Add a runtime recipe to restart nginx.  Can use from RS.

--- nginx/recipies/restart.rb
+++ nginx/recipies/restart.rb
@@ -1,1 +1,14 @@
-# TODO
+
+#
+# Stop, start nginx daemon
+#
+
+service "nginx" do
+  provider Chef::Provider::Service::Upstart
+  action :stop
+end
+
+service "nginx" do
+  provider Chef::Provider::Service::Upstart
+  action :start
+end
