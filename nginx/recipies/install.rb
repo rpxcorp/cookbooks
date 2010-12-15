8ea5b2e975f3b91559fbaefacb02e5e8a0749264
sysops <sysops@rpxcorp.com>
2010-12-14 17:56:41 -0800

    Added recipe for nginx.

---
+++ nginx/recipies/install.rb
@@ -0,0 +1,27 @@
+#
+# Install configure nginx
+#
+
+require_recipe "ruby_enterprise"
+require_recipe "passenger_enterprise"
+
+execute "build nginx from source" do
+  command "/opt/ruby-enterprise/bin/passenger-install-nginx-module --auto --auto-download --prefix=/opt/nginx"
+end
+
+template "/opt/nginx/conf/nginx.conf" do
+  source "nginx.conf.erb"
+end
+
+template "/etc/init/nginx.conf" do
+  source "nginx.upstart.erb"
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
+service "nginx" do
+  provider Chef::Provider::Service::Upstart
+  action :enable
+end
+
