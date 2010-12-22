6cfd278077a511f4fc290352d9db3f288d93549b
sysops <sysops@rpxcorp.com>
2010-12-22 14:11:40 -0800

    Update postgres recipe. Remove unused fields.

--- postgresql/metadata.rb
+++ postgresql/metadata.rb
@@ -1,9 +1,5 @@
-maintainer        "Opscode, Inc."
-maintainer_email  "cookbooks@opscode.com"
-license           "Apache 2.0"
 description       "Installs and configures postgresql for clients or servers"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version           "0.11.0"
 
 recipe            "postgresql", "Installs postgresql client package"
 recipe            "postgresql::client", "Installs postgresql client package(s)"
