679af7654281f301462ae56edd6735437424539b
sysops <sysops@rpxcorp.com>
2010-12-22 13:55:43 -0800

    Update postgres recipe metadata to reflect reality.

--- postgresql/metadata.rb
+++ postgresql/metadata.rb
@@ -5,12 +5,6 @@ description       "Installs and configures postgresql for clients or servers"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version           "0.11.0"
 
-recipe            "postgresql", "Empty, use one of the other recipes"
+recipe            "postgresql", "Installs postgresql client package"
 recipe            "postgresql::client", "Installs postgresql client package(s)"
 recipe            "postgresql::server", "Installs postgresql server packages, templates"
-recipe            "postgresql::redhat", "Installs postgresql server packages, redhat family style"
-recipe            "postgresql::server", "Installs postgresql server packages, debian family style"
-
-%w{rhel centos fedora ubuntu debian suse}.each do |os|
-  supports os
-end
