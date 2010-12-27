5f6e3df2891013a5b6c51fb7bbf17ab5e608c212
sysops <sysops@rpxcorp.com>
2010-12-27 15:20:15 -0800

    Cleanup the passenger recipe.  Cut out a lot of the stuff we dont really need.

--- passenger_enterprise/metadata.rb
+++ passenger_enterprise/metadata.rb
@@ -5,12 +5,5 @@ description      "Installs and configures Passenger under Ruby Enterprise Editio
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.4.2"
 
-recipe "passenger_enterprise", "Installs Passenger gem with Ruby Enterprise Edition"
-recipe "passenger_enterprise::apache2", "Enables Apache module configuration for passenger under Ruby Enterprise Edition"
-recipe "passenger_enterprise::nginx", "Installs Passenger gem w/ REE, and recompiles support into Nginx"
+recipe "passenger_enterprise::default", "Installs Passenger gem with Ruby Enterprise Edition"
 
-%w{ ruby_enterprise nginx apache2 }.each do |cb|
-  depends cb
-end
-
-supports "ubuntu"
--- passenger_enterprise/recipes/default.rb
+++ passenger_enterprise/recipes/default.rb
@@ -23,7 +23,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe "ruby_enterprise"
+require_recipe "ruby_enterprise"
 
 ree_gem "passenger" do
   version node[:passenger_enterprise][:version]
