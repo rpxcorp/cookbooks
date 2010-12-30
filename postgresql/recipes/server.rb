e7f000d75bbe0df0a10707dd2cd33ef121523934
sysops <sysops@rpxcorp.com>
2010-12-30 15:22:02 -0800

    Dressing up the PG server install with optiona; modules.

--- postgresql/recipes/server.rb
+++ postgresql/recipes/server.rb
@@ -16,16 +16,29 @@ pkg = [
   "postgresql-#{node.postgresql.version}",
   "postgresql-contrib-#{node.postgresql.version}",
 ]
-opt_pkg = [
-  "postgresql-8.3-orafce",
-  "postgresql-8.3-plruby",
-  "postgresql-#{node.postgresql.version}-plproxy",
-  "postgresql-#{node.postgresql.version}-postgis",
-  "postgresql-#{node.postgresql.version}-slony1",
-  "postgresql-autodoc",
-  "postgresql-filedump-#{node.postgresql.version}",
+opt = [
+  "ptop",
+  "pgtune",
+  #"pgagent",
+  #"postgresql-#{node.postgresql.version}-slony1",
+  #"slony1-bin",
+  #"pgpool2",
+  #"postgresql-#{node.postgresql.version}-plproxy",
+  #"postgresql-#{node.postgresql.version}-postgis",
+  #"pgloader",
+  ## Oracle compat
+  #"ora2pg",
+  #"postgresql-8.3-orafce",
+  ## Procedural Languages
+  #"postgresql-8.3-plruby",
+  #"postgresql-plperl-8.4",
+  #"postgresql-plpython-8.4",
+  ## Just stuff
+  #"postgresql-autodoc",
+  #"postgresql-filedump-#{node.postgresql.version}",
 ]
 
+pkg = pkg + opt
 pkg.each do |i|
   package "#{i}" do
     action :install
