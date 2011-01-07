1835f703e884e0eec5d1ca6a9b2f40b8a547ea89
sysops <sysops@rpxcorp.com>
2011-01-06 17:37:17 -0800

    Getting slony recipe to work.

--- postgresql/recipes/slony.rb
+++ postgresql/recipes/slony.rb
@@ -14,7 +14,19 @@ pkg.each do |i|
   package i
 end
 
+template "/etc/slony1/slon_tools.conf" do
+  source "slon_tools.conf.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
 service "postgresql-#{node.postgresql.version}" do
+  provider Chef::Provider::Service::Init::Debian
+  action :restart
+end
+
+service "slony1" do
   provider Chef::Provider::Service::Init::Debian
   action :restart
 end
