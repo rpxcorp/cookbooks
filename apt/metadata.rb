4ccd1e57d7f700309e5b7e134fa12fcdd30de3be
sysops <sysops@rpxcorp.com>
2011-01-14 09:51:08 -0800

    Metadata defs for new apt recipes.

--- apt/metadata.rb
+++ apt/metadata.rb
@@ -3,9 +3,11 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Configures apt and apt services"
 version           "0.9.1"
-recipe            "apt", "Runs apt-get update during compile phase and sets up preseed directories"
+recipe            "apt::default", "Runs apt-get update during compile phase and sets up preseed directories"
 recipe            "apt::cacher", "Set up an APT cache"
 recipe            "apt::proxy", "Set up an APT proxy"
+recipe            "apt::update", "Update all out of date apt packages"
+recipe            "apt::autoremove", "Remove any unused apt packages"
 
 %w{ ubuntu debian }.each do |os|
   supports os
