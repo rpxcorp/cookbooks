2a907dcb033b011fd0787d259a81d0504673c24b
sysops <sysops@rpxcorp.com>
2011-01-17 11:52:20 -0800

    New recipe to add PG statspack performance monitoring.

---
+++ postgresql/recipes/pgstatspack.rb
@@ -0,0 +1,20 @@
+
+#
+# PGStatspack
+#
+
+require_recipe "postgres::server"
+
+remote_file "/root/pgstatspack.tar.gz" do
+  source "http://pgfoundry.org/frs/download.php/2800/pgstatspack_version_2.2.tar.gz"
+do
+
+bash "install_pgstatspack" do
+  code <-EOH
+  tar xzf pgstatspack.tar.gz
+  cd pgstatspack
+  make install
+  EOH
+end
+
+# add crons, etc
