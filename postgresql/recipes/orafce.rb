0191fbd6d5e9541d0e8077144b919740c90f345c
sysops <sysops@rpxcorp.com>
2011-01-18 14:53:31 -0800

    Oopsie on orafce

--- postgresql/recipes/orafce.rb
+++ postgresql/recipes/orafce.rb
@@ -6,6 +6,8 @@
 
 require_recipe "postgresql::server"
 
+package "ora2pg"
+
 remote_file "/tmp/orafce.tar.gz" do
   source "http://pgfoundry.org/frs/download.php/2908/orafce-3.0.3.tar.gz"
   mode "0600"
@@ -16,8 +18,8 @@ bash "make_orafce" do
   code <<-EOH
     tar xzf orafce.tar.gz
     cd orafce
-    make USE_PGXS=1 install
     make USE_PGXS=1
+    make USE_PGXS=1 install
   EOH
 end
 
