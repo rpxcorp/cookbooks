f44246e5f677ad6da043e7d033d3334a7a7032e6
sysops <sysops@rpxcorp.com>
2011-01-24 17:33:19 -0800

    Make pgpool actually start with new recipe.

--- postgresql/recipes/pgpool.rb
+++ postgresql/recipes/pgpool.rb
@@ -11,7 +11,14 @@ template "/etc/pgpool.conf" do
   source "pgpool.conf.erb"
 end
 
-service "pgpool" do
+directory "/var/run/postgresql" do
+  owner "postgres"
+  group "root"
+  mode "0755"
+  action :create
+end
+
+service "pgpool2" do
   provider Chef::Provider::Service::Init::Debian
   action :restart
 end
