452b2ea5a606f6409f0bba04ac1cd10b76bc8c8c
sysops <sysops@rpxcorp.com>
2011-01-27 10:14:22 -0800

    Fixup PG recipes.

--- postgresql/recipes/pgagent.rb
+++ postgresql/recipes/pgagent.rb
@@ -8,6 +8,6 @@ require_recipe "postgresql::server"
 package "pgagent"
 
 service "postgresql" do
-  service_name VERSION == '9.0' ? 'postgresql' : "postgresql-#{node.postgresql.version}"
+  service_name "#{node.postgresql.version}" == '9.0' ? 'postgresql' : "postgresql-#{node.postgresql.version}"
   action :restart
 end
