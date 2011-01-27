86e91b3e54cc16bc5cebf1e26bf5385a007fd790
sysops <sysops@rpxcorp.com>
2011-01-27 13:20:46 -0800

    Fixup PG recipes.

--- postgresql/metadata.rb
+++ postgresql/metadata.rb
@@ -7,17 +7,18 @@ recipe            "postgresql::server", "Installs postgresql server packages, te
 recipe            "postgresql::restart", "Runtime script restarts postgresql daemon(s)."
 
 recipe            "postgresql::backport-9", "Installs backport for postgres 9.0."
-recipe            "postgresql::adminpack", "Installs server side adminpack for pgadmin tool."
+recipe            "postgresql::contrib", "Installs server side community contributions."
 recipe            "postgresql::pgagent", "Install pgagent job control system for postgres."
-recipe            "postgresql::pgpool", "Install pgpool connection pooler."
-recipe            "postgresql::pgpool-II", "Install pgpool-II connection pooler."
+recipe            "postgresql::pgpool", "Install pgpool connection pooler via apt."
+recipe            "postgresql::pgpool-II", "Install pgpool-II connection pooler from source."
 recipe            "postgresql::ripi-config", "Configure PG server for RIPI"
 recipe            "postgresql::slony", "Install Slony replication."
-recipe            "postgresql::orafce", "Install Orafce Oracle compat utils."
+recipe            "postgresql::statspack", "Install PG statspack."
+recipe            "postgresql::oracle-compat", "Install Orafce Oracle compat utils."
 
 attribute "postgresql/version",
   :display_name => "Postgres Version",
-  :description => "Determines which version of Postgres qill be installed.",
+  :description => "Determines which version of Postgres will be installed.",
   :required => "optional",
   :default => "8.4",
   :recipes => [ "postgresql::server", ]
